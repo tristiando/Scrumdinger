34ec2cb91ebeef00fbad32e207b095ac91c1137d
Thu Do <dothu.khtn@gmail.com>
2023-09-17 16:41:37 +0700

    Create project based on Apple original tutorial

---
+++ OriginalTutorial/Scrumdinger/Scrumdinger/MeetingView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Thu Do on 17/09/2023.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
