fd6398cefb6ed23a44989d7f8090b5f6c33e46dd
Thu Do <dothu.khtn@gmail.com>
2023-09-17 20:08:36 +0700

    Modify and style Meeting view

--- OriginalTutorial/Scrumdinger/Scrumdinger/MeetingView.swift
+++ OriginalTutorial/Scrumdinger/Scrumdinger/MeetingView.swift
@@ -10,10 +10,29 @@ import SwiftUI
 struct MeetingView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+            }
         }
         .padding()
     }
