ca8ee3018021776d89ef5d7127eed6ba224b8724
Thu Do <dothu.khtn@gmail.com>
2023-09-20 00:38:44 +0700

    Customize the label style

---
+++ OriginalTutorial/Scrumdinger/Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,23 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Thu Do on 20/09/2023.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
+
+
